82e6726e0eee5bcae79ec6929824cf9fefc45713
ans-cst <Cloudservices.Servicedesk@ansgroup.co.uk>
2018-05-03 15:42:46 +0100

    Fixing installation

--- easynanonode-bootstrap.sh
+++ easynanonode-bootstrap.sh
@@ -2,4 +2,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
-bash <(curl -s https://raw.githubusercontent.com/nanotools/easy-nano-node/master/install.sh)
+cd /root
+wget https://raw.githubusercontent.com/nanotools/easy-nano-node/master/install.sh
+sh install.sh
