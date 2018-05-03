f6491e6934be91596041e1e05f9005814a42b4b4
ans-cst <Cloudservices.Servicedesk@ansgroup.co.uk>
2018-05-03 16:12:40 +0100

    Adding Docker Compose Installation

--- easynanonode-bootstrap.sh
+++ easynanonode-bootstrap.sh
@@ -1,7 +1,14 @@
+#Install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce
+
+#Install Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+#Install Easy Nano Node
 cd /root
 wget https://raw.githubusercontent.com/nanotools/easy-nano-node/master/install.sh
 sh install.sh
